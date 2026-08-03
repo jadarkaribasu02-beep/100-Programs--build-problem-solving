755862264388ea4def59b8d97dfd16dd9a417c40
karibasu jadar <jadarkaribasu02@gmail.com>
2026-08-03 22:26:13 +0530

    slow leraner

--- Level 1 — Warm-up (Output, variables, basic input)/sixth.java
+++ Level 1 — Warm-up (Output, variables, basic input)/sixth.java
@@ -1,8 +1,26 @@
+//Swap two numbers without using a third variable
+
 import java.util.Scanner;
 public class sixth {
     public static void main (String[] args){
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter first number:");
+        int a = sc.nextInt();
+
+        System.out.println("enter second number:");
+        int b = sc.nextInt();
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
 
     }
 }
