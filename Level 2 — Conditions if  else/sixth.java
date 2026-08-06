cba7fb573cbce8d9e03a4886d75e050f794c0f1b
karibasu jadar <jadarkaribasu02@gmail.com>
2026-08-06 13:51:26 +0530

    x

---
+++ Level 2 — Conditions if  else/sixth.java
@@ -0,0 +1,20 @@
+//Check Whether a Character is a Vowel or Consonant
+
+import java.util.Scanner;
+
+public class sixth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
+
+        sc.close();
+    }
+}
