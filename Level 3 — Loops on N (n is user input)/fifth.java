a4f205bda8ee67c3bd7a5bc5b339ee975999d00b
karibasu jadar <jadarkaribasu02@gmail.com>
2026-08-09 20:01:13 +0530

    n

---
+++ Level 3 — Loops on N (n is user input)/fifth.java
@@ -0,0 +1,8 @@
+//Write a program to find the sum of all natural numbers from 1 to n.
+
+import java.util.Scanner;
+
+public class fifth {
+   
+    
+}
--- fifth.java
+++
@@ -1,8 +0,0 @@
-//Write a program to find the sum of all natural numbers from 1 to n.
-
-import java.util.Scanner;
-
-public class fifth {
-   
-    
-}
