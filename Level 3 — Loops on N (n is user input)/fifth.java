7d50b1f81669fe8a002bd683db556a3b567087e5
karibasu jadar <jadarkaribasu02@gmail.com>
2026-08-10 10:51:22 +0530

    n

--- Level 3 — Loops on N (n is user input)/fifth.java
+++ Level 3 — Loops on N (n is user input)/fifth.java
@@ -1,8 +1,27 @@
-//Write a program to find the sum of all natural numbers from 1 to n.
+//Write a program to find the sum of all even and odd numbers from 1 to n.
 
 import java.util.Scanner;
 
 public class fifth {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter number:");
+        int n = sc.nextInt();
+        int sum = 0;
+        int odd = 0;
+
+        for (int i = 1; i <=n; i++){
+            if(i % 2 == 0){
+                sum +=i;
+            }else {
+                odd +=i;
+            }
+        }
+        System.out.println(sum);
+        System.out.println(odd);
+        sc.close();
+    }
    
     
 }
